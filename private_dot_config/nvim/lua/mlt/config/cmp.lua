57ebe528481d83e54a3e87eb717d484fb8f9e3ef
Mathieu Le Tiec <mathieu.letiec+github@gmail.com>
2024-12-14 14:58:32 +0100

    Replace Neodev with Lazydev.nvim

--- private_dot_config/nvim/lua/mlt/config/cmp.lua
+++ private_dot_config/nvim/lua/mlt/config/cmp.lua
@@ -1,41 +1,41 @@
-local has_cmp, cmp = pcall(require, 'cmp')
+local has_cmp, cmp = pcall(require, "cmp")
 if not has_cmp then
-  mlt.notify_error 'cmp not found'
+  mlt.notify_error("cmp not found")
   return
 end
 
 local snippets_config = nil
 local format_config = nil
 
-local has_lspkind, lspkind = pcall(require, 'lspkind')
+local has_lspkind, lspkind = pcall(require, "lspkind")
 if not has_lspkind then
-  mlt.notify_error 'lspkind not found'
+  mlt.notify_error("lspkind not found")
 else
   format_config = {
-    format = lspkind.cmp_format {
-      mode = 'symbol_text',
-      before = require('tailwind-tools.cmp').lspkind_format,
+    format = lspkind.cmp_format({
+      mode = "symbol_text",
+      before = require("tailwind-tools.cmp").lspkind_format,
       menu = {
-        copilot = '[copilot]',
-        luasnip = '[snip]',
-        buffer = '[buf]',
-        nvim_lsp = '[LSP]',
-        nvim_lua = '[api]',
-        path = '[path]',
-        calc = '[calc]',
-        conjure = '[conj]',
+        copilot = "[copilot]",
+        luasnip = "[snip]",
+        buffer = "[buf]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[api]",
+        path = "[path]",
+        calc = "[calc]",
+        conjure = "[conj]",
       },
-    },
+    }),
   }
 end
 
 -- TODO: configure LuaSnip
-local has_luasnip, luasnip = pcall(require, 'luasnip')
+local has_luasnip, luasnip = pcall(require, "luasnip")
 if not has_luasnip then
-  mlt.notify_error 'LuaSnip not found'
+  mlt.notify_error("LuaSnip not found")
 else
-  require('luasnip/loaders/from_vscode').lazy_load()
-  luasnip.filetype_extend('ruby', { 'rails' })
+  require("luasnip/loaders/from_vscode").lazy_load()
+  luasnip.filetype_extend("ruby", { "rails" })
 
   snippets_config = {
     expand = function(args)
@@ -44,36 +44,46 @@ else
   }
 end
 
-cmp.setup {
+cmp.setup({
   snippet = snippets_config,
-  mapping = cmp.mapping.preset.insert {},
+  mapping = cmp.mapping.preset.insert({}),
   sources = {
-    { name = 'copilot' },
-    { name = 'luasnip' },
-    { name = 'nvim_lsp' },
-    { name = 'nvim_lua' },
-    { name = 'path' },
-    { name = 'buffer' },
-    { name = 'calc' },
-    { name = 'conjure' },
+    { name = "copilot" },
+    { name = "luasnip" },
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
+    { name = "path" },
+    { name = "buffer" },
+    { name = "calc" },
+    { name = "conjure" },
+    { name = "lazydev", group_index = 0 },
   },
   formatting = format_config,
-}
+})
 
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline {},
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline({}),
   completion = {
     keyword_length = 3,
   },
   sources = {
-    { name = 'path' },
-    { name = 'cmdline' },
+    { name = "path" },
+    { name = "cmdline" },
   },
 })
 
-cmp.setup.cmdline('/', {
-  mapping = cmp.mapping.preset.cmdline {},
+cmp.setup.cmdline("/", {
+  mapping = cmp.mapping.preset.cmdline({}),
   sources = {
-    { name = 'buffer' },
+    { name = "buffer" },
   },
-})
+})({ -- optional cmp completion source for require statements and module annotations
+      "hrsh7th/nvim-cmp",
+      opts = function(_, opts)
+        opts.sources = opts.sources or {}
+        table.insert(opts.sources, {
+          name = "lazydev",
+          group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+        })
+      end,
+    })
--- private_dot_config/nvim/lua/mlt/plugins/init.lua
+++ private_dot_config/nvim/lua/mlt/plugins/init.lua
@@ -1,105 +1,113 @@
 local specs = {
   -- Colorscheme
   {
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    "catppuccin/nvim",
+    name = "catppuccin",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd [[colorscheme catppuccin-mocha]]
+      vim.cmd([[colorscheme catppuccin-mocha]])
     end,
   }, -- colorscheme
 
   {
-    'folke/neodev.nvim',
-    desc = 'Neovim setup for init.lua and plugin development',
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    desc = "Neovim setup for init.lua and plugin development",
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
   },
 
   {
-    'lukas-reineke/indent-blankline.nvim',
+    "lukas-reineke/indent-blankline.nvim",
     config = function()
-      require 'mlt.config.indent-blankline'
+      require("mlt.config.indent-blankline")
     end,
   },
 
   -- LSP
-  { 'neovim/nvim-lspconfig', desc = 'Quickstart configs for Nvim LSP' },
-  { 'onsails/lspkind.nvim',  desc = 'vscode-like pictograms for neovim lsp completion items' },
+  { "neovim/nvim-lspconfig", desc = "Quickstart configs for Nvim LSP" },
+  { "onsails/lspkind.nvim",  desc = "vscode-like pictograms for neovim lsp completion items" },
   -- FIXME: No longer maintained, switch to none-ls
   {
-    'nvimtools/none-ls.nvim', --
-    desc = 'Bridge between non-LSP tooling and neovim LSP client',
+    "nvimtools/none-ls.nvim", --
+    desc = "Bridge between non-LSP tooling and neovim LSP client",
     config = function()
-      require 'mlt.config.none_ls'
+      require("mlt.config.none_ls")
     end,
   },
 
   {
-    'ThePrimeagen/refactoring.nvim',
-    desc = 'Provide Refactoring-book-inspired code actions for LSP',
+    "ThePrimeagen/refactoring.nvim",
+    desc = "Provide Refactoring-book-inspired code actions for LSP",
     dependencies = {
-      { 'nvim-lua/plenary.nvim' },
-      { 'nvim-treesitter/nvim-treesitter' },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-treesitter/nvim-treesitter" },
     },
   },
 
   -- Test
   {
-    'nvim-neotest/neotest', -- Test runner
+    "nvim-neotest/neotest", -- Test runner
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
-      'olimorris/neotest-rspec',
-      'nvim-neotest/nvim-nio',
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "olimorris/neotest-rspec",
+      "nvim-neotest/nvim-nio",
     },
     config = function()
-      require 'mlt.config.neotest'
+      require("mlt.config.neotest")
     end,
   },
 
   -- Git integration
-  'tpope/vim-fugitive',         -- Vim plugin for git
-  { 'idanarye/vim-merginal' },  -- Fugitive extension to manage and merge Git branches
-  { 'junegunn/gv.vim' },
-  { 'sindrets/diffview.nvim' }, -- Single tabpage interface for easily cycling through diffs for all modified files for any git rev.
+  "tpope/vim-fugitive",        -- Vim plugin for git
+  { "idanarye/vim-merginal" }, -- Fugitive extension to manage and merge Git branches
+  { "junegunn/gv.vim" },
+  { "sindrets/diffview.nvim" }, -- Single tabpage interface for easily cycling through diffs for all modified files for any git rev.
 
   -- Languages Support
 
   -- Ruby/Rails
-  { 'vim-ruby/vim-ruby' }, -- TODO: Document textobjects and motions
-  { 'tpope/vim-bundler' },
-  { 'tpope/vim-rails' },
-  { 'slim-template/vim-slim' },
+  { "vim-ruby/vim-ruby" }, -- TODO: Document textobjects and motions
+  { "tpope/vim-bundler" },
+  { "tpope/vim-rails" },
+  { "slim-template/vim-slim" },
 
   -- Javascript
-  { 'yuezk/vim-js' },
-  { 'HerringtonDarkholme/yats' },
-  { 'maxmellon/vim-jsx-pretty' },
+  { "yuezk/vim-js" },
+  { "HerringtonDarkholme/yats" },
+  { "maxmellon/vim-jsx-pretty" },
 
   -- Clojure and other lisps
   {
-    'guns/vim-sexp',
+    "guns/vim-sexp",
     config = function()
-      require 'mlt.config.sexp'
+      require("mlt.config.sexp")
     end,
   },
-  { 'tpope/vim-sexp-mappings-for-regular-people' },
+  { "tpope/vim-sexp-mappings-for-regular-people" },
   {
-    'Olical/conjure',
+    "Olical/conjure",
     config = function()
-      vim.g['conjure#mapping#prefix'] = ',c'
+      vim.g["conjure#mapping#prefix"] = ",c"
     end,
   },
 
   -- tailwind-tools.lua
   {
-    'luckasRanarison/tailwind-tools.nvim',
-    name = 'tailwind-tools',
-    build = ':UpdateRemotePlugins',
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
     dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-telescope/telescope.nvim', -- optional
-      'neovim/nvim-lspconfig',         -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig",      -- optional
     },
     opts = {
       server = {
@@ -129,56 +137,56 @@ local specs = {
   },
 
   {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
     config = function()
-      require 'mlt.config.treesitter'
+      require("mlt.config.treesitter")
     end,
   },
-  { 'nvim-treesitter/playground' },
+  { "nvim-treesitter/playground" },
   {
-    'RRethy/nvim-treesitter-endwise',
+    "RRethy/nvim-treesitter-endwise",
     -- FIXME: this should go in the treesitter configuration
     config = function()
-      require('nvim-treesitter.configs').setup { endwise = { enable = true } }
+      require("nvim-treesitter.configs").setup({ endwise = { enable = true } })
     end,
   },
 
   -- Completion
   {
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     config = function()
-      require 'mlt.config.cmp'
+      require("mlt.config.cmp")
     end,
   },
-  { 'hrsh7th/cmp-buffer' },
-  { 'hrsh7th/cmp-path' },
-  { 'hrsh7th/cmp-nvim-lua' },
-  { 'hrsh7th/cmp-nvim-lsp' },
-  { 'hrsh7th/cmp-calc' },
-  { 'hrsh7th/cmp-cmdline' },
-  { 'saadparwaiz1/cmp_luasnip' },
-  { 'PaterJason/cmp-conjure' },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-nvim-lua" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-calc" },
+  { "hrsh7th/cmp-cmdline" },
+  { "saadparwaiz1/cmp_luasnip" },
+  { "PaterJason/cmp-conjure" },
   {
-    'zbirenbaum/copilot-cmp',
+    "zbirenbaum/copilot-cmp",
     config = function()
-      require('copilot_cmp').setup()
+      require("copilot_cmp").setup()
     end,
   },
 
   -- Snippets
-  { 'L3MON4D3/LuaSnip' },
-  { 'rafamadriz/friendly-snippets' },
+  { "L3MON4D3/LuaSnip" },
+  { "rafamadriz/friendly-snippets" },
   {
-    'zbirenbaum/copilot.lua',
-    cmd = 'Copilot',
-    event = 'InsertEnter',
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
     config = function()
-      require('copilot').setup {
+      require("copilot").setup({
         suggestion = { enabled = false },
         panel = { enabled = false },
         filetypes = { markdown = true },
-      }
+      })
     end,
   },
 
@@ -188,40 +196,40 @@ local specs = {
   -- { 'kana/vim-textobj-entire' }, -- Text objects for entire buffer
   -- { 'kana/vim-textobj-indent' }, -- text objects to select a block of lines similarly indented to the current line
 
-  { 'tpope/vim-surround' }, -- Delete/change/add parentheses/quotes/XML-tags/much more with ease
-  { 'tpope/vim-abolish' },  -- Easily search for, substitute, and abbreviate multiple variants of a word
+  { "tpope/vim-surround" }, -- Delete/change/add parentheses/quotes/XML-tags/much more with ease
+  { "tpope/vim-abolish" }, -- Easily search for, substitute, and abbreviate multiple variants of a word
 
   -- Telescope
   {
-    'nvim-telescope/telescope.nvim',
-    desc = 'highly extendable fuzzy finder over lists',
+    "nvim-telescope/telescope.nvim",
+    desc = "highly extendable fuzzy finder over lists",
     config = function()
-      require 'mlt.config.telescope'
+      require("mlt.config.telescope")
     end,
-  },                                                              --
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }, -- FZF sorter for telescope
+  },                                                             --
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- FZF sorter for telescope
 
   -- FIXME: replace with Heirline
   {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
     config = function()
-      require 'mlt.config.lualine'
+      require("mlt.config.lualine")
     end,
   },
 
-  { 'mattn/emmet-vim' }, -- Emmet support
+  { "mattn/emmet-vim" }, -- Emmet support
   {
-    'stefandtw/quickfix-reflector.vim',
-    desc = 'Edit in the quickfix window, save modifications in actual files',
+    "stefandtw/quickfix-reflector.vim",
+    desc = "Edit in the quickfix window, save modifications in actual files",
     config = function()
-      require 'mlt.config.quickfix-reflector'
+      require("mlt.config.quickfix-reflector")
     end,
   },
 
   {
-    'preservim/tagbar',
-    desc = 'provides an easy way to browse the tags of the current file and get an overview of its structure',
+    "preservim/tagbar",
+    desc = "provides an easy way to browse the tags of the current file and get an overview of its structure",
   },
   -- {
   --   'junegunn/vim-peekaboo',
@@ -232,51 +240,51 @@ local specs = {
   -- },
 
   {
-    'folke/todo-comments.nvim', -- Highlight, list and search todo comments in your projects
+    "folke/todo-comments.nvim", -- Highlight, list and search todo comments in your projects
     config = function()
-      require 'mlt.config.todo-comments'
+      require("mlt.config.todo-comments")
     end,
   },
-  { 'tpope/vim-dispatch' },
-  { 'tpope/vim-unimpaired' },
-  { 'tpope/vim-repeat' },
+  { "tpope/vim-dispatch" },
+  { "tpope/vim-unimpaired" },
+  { "tpope/vim-repeat" },
 
-  { 'simeji/winresizer' },
-  { 'mbbill/undotree' },
+  { "simeji/winresizer" },
+  { "mbbill/undotree" },
 
-  { 'jlanzarotta/bufexplorer' },
-  { 'junegunn/vim-slash',        desc = 'Enhancing in-buffer search experience' },
-  { 'easymotion/vim-easymotion', desc = 'Vim motions on speed!' },
-  { 'wellle/targets.vim',        desc = 'Provides additional text objects' },
-  { 'kshenoy/vim-signature',     desc = 'Place, toggle and display marks.' },
+  { "jlanzarotta/bufexplorer" },
+  { "junegunn/vim-slash",        desc = "Enhancing in-buffer search experience" },
+  { "easymotion/vim-easymotion", desc = "Vim motions on speed!" },
+  { "wellle/targets.vim",        desc = "Provides additional text objects" },
+  { "kshenoy/vim-signature",     desc = "Place, toggle and display marks." },
 
-  { 'AndrewRadev/splitjoin.vim' },
-  { 'junegunn/vim-easy-align' },
-  { 'tomtom/tcomment_vim' },
-  { 'airblade/vim-gitgutter' },
+  { "AndrewRadev/splitjoin.vim" },
+  { "junegunn/vim-easy-align" },
+  { "tomtom/tcomment_vim" },
+  { "airblade/vim-gitgutter" },
 
   {
-    'jiangmiao/auto-pairs',
-    desc = 'Vim plugin, insert or delete brackets, parens, quotes in pair',
+    "jiangmiao/auto-pairs",
+    desc = "Vim plugin, insert or delete brackets, parens, quotes in pair",
     config = function()
-      require 'mlt.config.auto-pairs'
+      require("mlt.config.auto-pairs")
     end,
   },
 
   -- File explorer
   {
-    'stevearc/oil.nvim',
+    "stevearc/oil.nvim",
     opts = {},
-    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
   },
   -- TODO: Associate keymaps
   {
-    'danymat/neogen',
-    desc = 'Code annotation generator',
+    "danymat/neogen",
+    desc = "Code annotation generator",
     config = function()
-      require('neogen').setup { enable = true, snippet_engine = 'luasnip' }
+      require("neogen").setup({ enable = true, snippet_engine = "luasnip" })
     end,
-    dependencies = 'nvim-treesitter/nvim-treesitter',
+    dependencies = "nvim-treesitter/nvim-treesitter",
   },
 }
 
