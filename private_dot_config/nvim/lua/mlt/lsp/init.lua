f0dde191135154a95273d45f6ec2043ca9b52b4e
Mathieu Le Tiec <mathieu.letiec+github@gmail.com>
2024-12-15 18:19:50 +0100

    AutoPush -> AutoCommit

--- private_dot_config/nvim/lua/mlt/lsp/init.lua
+++ private_dot_config/nvim/lua/mlt/lsp/init.lua
@@ -1,7 +1,7 @@
 local lspconfig_found, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_found then
-  mlt.notify_error("nvim-lspconfig not found")
-  return
+	mlt.notify_error("nvim-lspconfig not found")
+	return
 end
 
 -- Enable trace logging
@@ -12,24 +12,24 @@ local handlers = require("mlt.lsp.handlers")
 handlers.setup()
 
 local default_config = {
-  on_attach = handlers.on_attach,
-  capabilities = handlers.capabilities,
+	on_attach = handlers.on_attach,
+	capabilities = handlers.capabilities,
 }
 
 -- Ruby
 require("lspconfig").ruby_lsp.setup({})
 
 -- Lua
--- require("mlt.lsp.servers.sumneko_lua").setup(lspconfig, default_config)
+require("mlt.lsp.servers.lua_ls").setup(lspconfig, default_config)
 
 -- Clojure
-require("mlt.lsp.servers.clojure_lsp").setup(lspconfig, default_config)
+require("mlt.lsp.servers.clojure_ls").setup(lspconfig, default_config)
 
 -- HTML
-require("mlt.lsp.servers.html").setup(lspconfig, default_config)
+require("mlt.lsp.servers.html_ls").setup(lspconfig, default_config)
 
 -- CSS
-require("mlt.lsp.servers.cssls").setup(lspconfig, default_config)
+require("mlt.lsp.servers.css_ls").setup(lspconfig, default_config)
 
 -- Golang
 require("lspconfig").golangci_lint_ls.setup({})
@@ -44,28 +44,28 @@ require("lspconfig").jsonls.setup({})
 local tailwind_default_config = lspconfig.tailwindcss.config_def.default_config
 local tailwind_filetypes = {}
 for k, v in pairs(tailwind_default_config.filetypes) do
-  tailwind_filetypes[k] = v
+	tailwind_filetypes[k] = v
 end
 table.insert(tailwind_filetypes, "ruby")
 
 local tailwind_config = vim.tbl_deep_extend("force", default_config, {
-  root_dir = lspconfig.util.root_pattern("config/tailwind.config.js", "tailwind.config.js", "tailwind.config.ts"),
-  filetypes = tailwind_filetypes,
-  settings = {
-    tailwindCSS = {
-      includeLanguages = {
-        ruby = "html",
-      },
-      experimental = {
-        classRegex = {
-          [[class: "([^"]*)]],
-          [[class: '([^']*)]],
-          [[form_class: "([^"]*)]],
-          [[form_class: '([^']*)]],
-        },
-      },
-    },
-  },
+	root_dir = lspconfig.util.root_pattern("config/tailwind.config.js", "tailwind.config.js", "tailwind.config.ts"),
+	filetypes = tailwind_filetypes,
+	settings = {
+		tailwindCSS = {
+			includeLanguages = {
+				ruby = "html",
+			},
+			experimental = {
+				classRegex = {
+					[[class: "([^"]*)]],
+					[[class: '([^']*)]],
+					[[form_class: "([^"]*)]],
+					[[form_class: '([^']*)]],
+				},
+			},
+		},
+	},
 })
 
 lspconfig.tailwindcss.setup(tailwind_config)
