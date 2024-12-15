24d57d723fbe11dfe1d7b24270f1f3c78d58c64d
Mathieu Le Tiec <mathieu.letiec+github@gmail.com>
2024-12-15 18:21:27 +0100

    test

--- private_dot_config/nvim/lua/mlt/lsp/servers/lua_ls.lua
+++ private_dot_config/nvim/lua/mlt/lsp/servers/lua_ls.lua
@@ -1,23 +1,23 @@
 local M = {}
 
 M.setup = function(lspconfig, default_config)
-  local config = vim.tbl_deep_extend('force', default_config, {
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { 'vim' },
-        },
-        workspace = {
-          library = {
-            [vim.fn.stdpath 'config' .. '/lua'] = true,
-            '${3rd}/luv/library',
-          },
-        },
-      },
-    },
-  })
+	local config = vim.tbl_deep_extend("force", default_config, {
+		-- settings = {
+		--   Lua = {
+		--     diagnostics = {
+		--       globals = { 'vim' },
+		--     },
+		--     workspace = {
+		--       library = {
+		--         [vim.fn.stdpath 'config' .. '/lua'] = true,
+		--         '${3rd}/luv/library',
+		--       },
+		--     },
+		--   },
+		-- },
+	})
 
-  lspconfig.lua_ls.setup(config)
+	lspconfig.lua_ls.setup(config)
 end
 
 return M
