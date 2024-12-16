57cc05d7ef850eb59709de41e06e2218dd780b27
Mathieu Le Tiec <mathieu.letiec+github@gmail.com>
2024-12-16 10:56:23 +0100

    Move .viminfo to standard path

--- private_dot_config/nvim/lua/mlt/options.lua
+++ private_dot_config/nvim/lua/mlt/options.lua
@@ -1,5 +1,5 @@
 vim.opt.termguicolors = true -- enable terminal colors
-vim.opt.background = 'dark'  -- set background color to dark
+vim.opt.background = "dark"  -- set background color to dark
 
 vim.opt.number = true        -- Show line numbers
 vim.opt.cursorline = true    -- highlight current line
@@ -14,11 +14,13 @@ vim.opt.shiftwidth = 2       -- set tab width to 2
 vim.opt.softtabstop = 2      -- set soft tab width to 2, does not mess with tabstop, cf :h tabstop
 
 vim.opt.undofile = true      -- enable undo
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 vim.opt.laststatus = 2
 
+vim.opt.viminfofile = vim.fn.stdpath("state") .. "/viminfo"
+
 -- Use rg for :grep
-if vim.fn.executable 'rg' then
-  vim.opt.grepprg = 'rg -H --no-heading --vimgrep'
-  vim.opt.grepformat = '%f:%l:%c:%m'
+if vim.fn.executable("rg") then
+  vim.opt.grepprg = "rg -H --no-heading --vimgrep"
+  vim.opt.grepformat = "%f:%l:%c:%m"
 end
