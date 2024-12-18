288737b03ebcc625fedd7f5ae707c56258dff3fe
Mathieu Le Tiec <mathieu.letiec+github@gmail.com>
2024-12-18 09:51:32 +0100

    Add basic ruby Struct snippet

--- private_dot_config/nvim/after/ftplugin/ruby.lua
+++ private_dot_config/nvim/after/ftplugin/ruby.lua
@@ -11,4 +11,8 @@ ls.add_snippets("ruby", {
   s("let", fmt("let(:<>) { <> }", { i(1), i(0) }, { delimiters = "<>" })),
 })
 
+ls.add_snippets("ruby", {
+  s("struct", fmt("<> = Struct.new(<>) <>", { i(1), i(2), i(0) }, { delimiters = "<>" })),
+})
+
 vim.b.AutoPairs = vim.fn.AutoPairsDefine({ ["|"] = "|" })
